6035d5c5033860c8e0954c1f219414ea3d942070
Shaun Czubkowski <shaunczubkowski@outlook.com>
2021-06-19 11:56:56 -0500

    Move Spinner component to new shared folder.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SpinnerComponent } from './spinner/spinner.component';
+import { SpinnerComponent } from './components/spinner/spinner.component';
 import {MaterialModule} from "./material.module";
+import { IsActivePipe } from './pipes/is-active.pipe';
 
 @NgModule({
-  declarations: [SpinnerComponent],
+  declarations: [SpinnerComponent, IsActivePipe],
   imports: [
     CommonModule,
     MaterialModule
   ],
-  exports: [SpinnerComponent],
+  exports: [SpinnerComponent, IsActivePipe],
 })
 export class SharedModule {}
